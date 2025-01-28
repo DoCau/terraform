95de31485583f6cf9eb8617109b9a7377a9820c9
DoCau <work.docau@gmail.com>
2025-01-28 23:53:41 +0700

    Update security group ingress & egress to connect to the internet

--- modules/main.tf
+++ modules/main.tf
@@ -99,6 +99,13 @@ resource "aws_security_group" "ec2-security-group" {
     cidr_blocks = [var.vpc_cidr_block]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -111,6 +118,13 @@ resource "aws_security_group" "ec2-security-group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = [local.local_machine_cidr]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
